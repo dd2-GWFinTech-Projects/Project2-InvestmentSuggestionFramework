060a19e0b24203a6e7cf171bbd00e8b01f126608
David G <davidrgayman@gmail.com>
2021-01-11 18:54:10 -0500

    Committing changes before pycharm projject.

--- src/lib/06-valuation/ValuationCalculator.py
+++ src/lib/06-valuation/ValuationCalculator.py
@@ -133,6 +133,6 @@ class ValuationCalculator:
     def test__compute_value__dcf():
         npv_actual = compute_value__dcf(
             ebitda_projection = [ -645000.00, 189430.00, 183115.00, 187266.00, 191375.00, 195432.00, 199427.00, 203348.00, 207184.00, 210923.00, 214550.00 ],
-            r=0.08)
+            wacc=0.08)
         npv_expected = 621178.98 
 
