917746f1e573d2324e983b2ad95285fffac4bf78
David G <davidrgayman@gmail.com>
2021-01-13 13:54:01 -0500

    StockScoreContainer working.

--- src/lib/datastructures/StockScoreContainer.py
+++ src/lib/datastructures/StockScoreContainer.py
@@ -6,10 +6,11 @@ class StockScoreContainer:
         self.stock_score_map = {}
 
     def add(self, ticker, score, analysis_source):
-        if not analysis_source in self.stock_score_map:
+        if not (analysis_source in self.stock_score_map):
             self.stock_score_map[analysis_source] = []
-        else:
-            self.stock_score_map[analysis_source].append(StockScore(ticker, score))
+        self.stock_score_map[analysis_source].append(StockScore(ticker, score))
 
     def get_all_stockscores(self):
-        return list(self.stock_score_map.values())
+        values_nested_list = self.stock_score_map.values()
+        return [item for sublist in values_nested_list for item in sublist]
+
