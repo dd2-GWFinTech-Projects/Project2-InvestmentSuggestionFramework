aa4a45aff8e281e3e5737b344943f043e1ab5c92
nazarmilad@gmail.com <69940276+miladnazar@users.noreply.github.com>
2021-01-18 18:17:58 -0500

    New Changes

--- Price-Forecasting/Solved/app.py
+++ Price-Forecasting/Solved/app.py
@@ -1,13 +1,13 @@
 # Import libraries and dependencies
 import pandas as pd
 import plotly.express as px
-import plotly.graph_objects as go
+import plotly.graph_objs as go
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
-app = dash.Dash(__name__)
+app = dash.Dash()
 
 df = pd.read_csv('../Resources/ATLAS.csv')
 
