cb2a6be0e52d0be580248262fbacfeeaad6ef2d7
David G <davidrgayman@gmail.com>
2021-01-26 15:53:18 -0500

    Building portfolio builder test.

--- src/main/portfoliobuilder/PortfolioBuilder.py
+++ src/main/portfoliobuilder/PortfolioBuilder.py
@@ -56,40 +56,59 @@ class PortfolioBuilder:
     # --------------------------------------------------------------------------
 
 
-    def __compute_composite_scores(self, customer_metrics, stock_score_container):
-        return stock_score_container
-        # portfolio_composite_scores = {}
-        # for analysis_source in stock_score_container.stock_info_map.keys():
-        #
-        #     w = self.__weighting[analysis_source]
-        #
-        #     # Accumulate raw scores across all analysis methods
-        #     for stock_score in stock_score_container.stock_info_map[analysis_source]:
-        #         ticker = stock_score.ticker
-        #         raw_score = stock_score.score
-        #         if not (ticker in portfolio_composite_scores):
-        #             portfolio_composite_scores[ticker] = StockInfo(ticker, w * raw_score)
-        #         else:
-        #             old_score = portfolio_composite_scores[ticker]
-        #             portfolio_composite_scores[ticker] = StockInfo(ticker, w * raw_score + old_score.score)
-        #
-        # # Sort
-        # return list(portfolio_composite_scores.values())
+    def __compute_composite_scores(self, customer_metrics, stock_info_container):
+
+        # Ticker -> Composite Score dictionary
+        portfolio_composite_scores = {}
+
+        # Number of raw scores used to compute composite
+        portfolio_composite_score_counts = {}
+
+        # Sum the weighted scores
+        for stock_score in stock_info_container.get_all_scores_single_level():
+
+            stock_ticker = stock_score.get_ticker()
+            score = stock_score.get_score()
+            analysis_source = stock_score.get_analysis_source()
+            w = self.__weighting[analysis_source]
+
+            # Initialize dictionaries
+            if not (stock_ticker in portfolio_composite_scores):
+                portfolio_composite_scores[stock_ticker] = 0
+            if not (stock_ticker in portfolio_composite_score_counts):
+                portfolio_composite_score_counts[stock_ticker] = 0
+
+            # Add scores
+            portfolio_composite_scores[stock_ticker] += w * score
+            portfolio_composite_score_counts[stock_ticker] += 1
+
+        # Average over the number of analysis methods
+        for stock_ticker in portfolio_composite_scores.keys():
+            portfolio_composite_scores[stock_ticker] /= portfolio_composite_score_counts[stock_ticker]
+
+        return list(portfolio_composite_scores.values())
 
 
     def __sort_stock_score_list(self, score_list):
         def score_sort(stock_score):
-            return stock_score.score
+            return stock_score.get_score()
         score_list.sort(reverse=True, key=score_sort)
         return score_list
 
 
     def __compute_shares(self, customer_metrics, stock_score_list):
-        # TODO Calculate price * nshares to equal investment
-        total_nbr_shares = 400 #customer_metrics.investmentAmount
-        stock_shares_list = []
-        # for stock_score in stock_score_list:
-        #     ticker = stock_score.ticker
-        #     score = stock_score.score
-        #     stock_shares_list.append(StockInfo(ticker, 100))
-        return stock_shares_list
+
+        total_nbr_shares = 400  #customer_metrics.investmentAmount
+
+        # Compute total score
+        total_score = 0.0
+        for stock_score in stock_score_list:
+            total_score += stock_score.get_score()
+
+        # Scale shares proportionally   #TODO this is wrong bc it does not take money into account
+        portfolio = {}
+        for stock_score in stock_score_list:
+            num_shares = total_nbr_shares * stock_score.get_score() / total_score
+            portfolio[stock_score.get_ticker()] = num_shares
+
+        return portfolio
