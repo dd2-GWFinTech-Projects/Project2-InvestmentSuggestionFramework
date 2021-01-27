2abcf4f7dbd1514e33b3fba189fd2a4dce209b62
david <drg.developer.2@gmail.com>
2021-01-27 17:27:55 -0500

    Ignoring local dependency package dir.

--- src/build-and-deploy-aws-package.sh
+++ src/build-and-deploy-aws-package.sh
@@ -15,7 +15,11 @@
 
 # Make virtual environment
 #virtualenv venv
-source venv/bin/activate
+#source venv/bin/activate
+
+# Package the code
+zip litquidity-chatbot.zip -r lambda_function.py main/
+#zip litquidity-chatbot.zip -r lambda_function.py main/ .package/
 
 # Download dependencies to local directory - Targeted directory
 pip install --target ./.package requests
@@ -23,57 +27,10 @@ pip install --target ./.package python-dotenv
 pip install --target ./.package alpaca-trade-api
 pip install --target ./.package pandas
 
-# Install dependencies - Virtual Environment
-#pip install python-dotenv
-#pip install alpaca-trade-api
-#pip install requests
-#pip install pandas
-##pip install scikit-learn
-##pip install imbalanced-learn
-##pip install pydotplus
-##pip install spacy
-##pip install python -m spacy download en_core_web_sm
-##pip install imblearn
-##pip install psycopg2
-##pip install arch
-##pip install intake
-##pip install intake-parquet
-##pip install wordcloud
-##pip install tabulate
-##pip install asyncio
-##pip install iexfinance
-##pip install tesseract
-##pip install chatterbot
-##pip install --upgrade tensorflow
-##npm install dotenv
-##pip install newsapi-python==0.2.5
+# Add the dependencies to the package root
+cd .package
+zip -r ../litquidity-chatbot.zip .
+cd ..
 
-
-
-
-
-zip litquidity-chatbot.zip -r lambda_function.py main/ package/
-
-#zip litquidity-chatbot.zip lambda_function.py \
-#    ./main/__init__.py \
-#    ./main/balancesheetgetter/BalanceSheetGetter.py \
-#    ./main/balancesheetgetter/__init__.py \
-#    ./main/datastructures/AnalysisMethod.py \
-#    ./main/datastructures/CustomerMetrics.py \
-#    ./main/datastructures/StockFinancialMetadata.py \
-#    ./main/datastructures/StockInfoContainer.py \
-#    ./main/datastructures/StockScore.py \
-#    ./main/datastructures/__init__.py \
-#    ./main/portfoliobuilder/PortfolioBuilder.py \
-#    ./main/portfoliobuilder/__init__.py \
-#    ./main/priceanalysis/PriceForecaster.py \
-#    ./main/priceanalysis/__init__.py \
-#    ./main/pricegetter/PriceGetter.py \
-#    ./main/pricegetter/__init__.py \
-#    ./main/stockfilter/StockFilter.py \
-#    ./main/stockfilter/__init__.py \
-#    ./main/valuation/ValuationCalculator.py \
-#    ./main/valuation/__init__.py \
-#    package
-
+# Upload and deploy the function code to AWS Lambda
 aws lambda update-function-code --function-name project2_get_recommended_portfolio --zip-file fileb://litquidity-chatbot.zip
