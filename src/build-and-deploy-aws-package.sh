715b0fb2ce60297606fa2238d0bf54b11cc9518f
david <drg.developer.2@gmail.com>
2021-01-27 18:43:02 -0500

    Updates to builder.

--- src/build-and-deploy-aws-package.sh
+++ src/build-and-deploy-aws-package.sh
@@ -18,15 +18,18 @@
 #source venv/bin/activate
 
 # Package the code
+mv litquidity-chatbot.zip .old.litquidity-chatbot.zip
 zip litquidity-chatbot.zip -r lambda_function.py main/
 #zip litquidity-chatbot.zip -r lambda_function.py main/ .package/
 
 # Download dependencies to local directory - Targeted directory
-pip install --target ./.package requests
-pip install --target ./.package python-dotenv
-pip install --target ./.package alpaca-trade-api
-pip install --target ./.package pandas
-pip install --target ./.package statsmodels
+if [[ ! 0 ]]; then
+	pip install --target ./.package requests
+	pip install --target ./.package python-dotenv
+  pip install --target ./.package alpaca-trade-api
+  pip install --target ./.package pandas
+  pip install --target ./.package statsmodels
+fi
 
 # Add the dependencies to the package root
 cd .package
@@ -34,4 +37,5 @@ zip -r ../litquidity-chatbot.zip .
 cd ..
 
 # Upload and deploy the function code to AWS Lambda
-aws lambda update-function-code --function-name project2_get_recommended_portfolio --zip-file fileb://litquidity-chatbot.zip
+#aws lambda update-function-code --function-name project2_get_recommended_portfolio --zip-file fileb://litquidity-chatbot.zip
+
