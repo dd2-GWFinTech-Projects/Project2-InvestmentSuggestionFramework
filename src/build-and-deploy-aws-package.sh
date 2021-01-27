f4a26ff127fbac8175abfe2111c2021b4c8fc514
david <drg.developer.2@gmail.com>
2021-01-27 15:01:07 -0500

    Developing build deploy script.

--- src/build-and-deploy-aws-package.sh
+++ src/build-and-deploy-aws-package.sh
@@ -2,33 +2,46 @@
 
 # Description: https://docs.aws.amazon.com/lambda/latest/dg/python-package.html
 
+# Initial configuration
+## Install AWS CLI
+#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+#unzip awscliv2.zip
+#sudo ./aws/install
 
+## Configure AWS CLI
+#aws configure
+
+
+# Download dependencies to local directory
 pip install --target ./package requests
 
 
 
 
 
+zip litquidity-chatbot.zip -r lambda_function.py main/ package/
 
-zip litquidity-chatbot.zip lambda_function.py \
-    ./main/__init__.py \
-    ./main/balancesheetgetter/BalanceSheetGetter.py \
-    ./main/balancesheetgetter/__init__.py \
-    ./main/datastructures/AnalysisMethod.py \
-    ./main/datastructures/CustomerMetrics.py \
-    ./main/datastructures/StockFinancialMetadata.py \
-    ./main/datastructures/StockInfoContainer.py \
-    ./main/datastructures/StockScore.py \
-    ./main/datastructures/__init__.py \
-    ./main/portfoliobuilder/PortfolioBuilder.py \
-    ./main/portfoliobuilder/__init__.py \
-    ./main/priceanalysis/PriceForecaster.py \
-    ./main/priceanalysis/__init__.py \
-    ./main/pricegetter/PriceGetter.py \
-    ./main/pricegetter/__init__.py \
-    ./main/stockfilter/StockFilter.py \
-    ./main/stockfilter/__init__.py \
-    ./main/valuation/ValuationCalculator.py \
-    ./main/valuation/__init__.py
+#zip litquidity-chatbot.zip lambda_function.py \
+#    ./main/__init__.py \
+#    ./main/balancesheetgetter/BalanceSheetGetter.py \
+#    ./main/balancesheetgetter/__init__.py \
+#    ./main/datastructures/AnalysisMethod.py \
+#    ./main/datastructures/CustomerMetrics.py \
+#    ./main/datastructures/StockFinancialMetadata.py \
+#    ./main/datastructures/StockInfoContainer.py \
+#    ./main/datastructures/StockScore.py \
+#    ./main/datastructures/__init__.py \
+#    ./main/portfoliobuilder/PortfolioBuilder.py \
+#    ./main/portfoliobuilder/__init__.py \
+#    ./main/priceanalysis/PriceForecaster.py \
+#    ./main/priceanalysis/__init__.py \
+#    ./main/pricegetter/PriceGetter.py \
+#    ./main/pricegetter/__init__.py \
+#    ./main/stockfilter/StockFilter.py \
+#    ./main/stockfilter/__init__.py \
+#    ./main/valuation/ValuationCalculator.py \
+#    ./main/valuation/__init__.py \
+#    package
 
 aws lambda update-function-code --function-name project2_get_recommended_portfolio --zip-file fileb://litquidity-chatbot.zip
+
